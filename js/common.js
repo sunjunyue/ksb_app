c046f5f19d3279936980a0deb1afac5221b74342
happyfather2009 <happyfather2009@163.com>
2017-08-11 18:31:47 +0800

    m

--- js/common.js
+++ js/common.js
@@ -1,1 +1,4 @@
-var url = 'http://192.168.31.243/ksbapi2/api/';
+var url = 'http://192.168.1.6/ksbapi2/api/';
+var url2 = 'http://192.168.1.6/ksbapi2/public/qrcodes/';
+var urlqiniu = 'http://osyuuevsn.bkt.clouddn.com/';
+var uploadtoken = 'cC_r55D0VmEpqE9eZxLLW8vRaJ1kZA0L_ynyTpEW:nm2fiC_KYLWRH8xANpMJCnakQZ0=:eyJzY29wZSI6ImtzYi11c2VyLXBob3RvIiwiZGVhZGxpbmUiOjE1MDI0NTc0Mjl9';
