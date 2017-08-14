2d3755283af12ca13a870304498c680b62ff5d6a
happyfather2009 <happyfather2009@163.com>
2017-08-14 14:44:46 +0800

    m

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-var url = 'http://192.168.1.9/ksbapi2/api/';
-var url2 = 'http://192.168.1.9/ksbapi2/public/qrcodes/';
+var url = 'http://192.168.1.15/ksbapi2/api/';
+var url2 = 'http://192.168.1.15/ksbapi2/public/qrcodes/';
 var urlqiniu = 'http://osyuuevsn.bkt.clouddn.com/';
-var uploadtoken = 'cC_r55D0VmEpqE9eZxLLW8vRaJ1kZA0L_ynyTpEW:g12TQIb1h-WD8WiMfXSYPjPuNdM=:eyJzY29wZSI6ImtzYi11c2VyLXBob3RvIiwiZGVhZGxpbmUiOjE1MDI1NDQ3NzF9';
+var uploadtoken = 'cC_r55D0VmEpqE9eZxLLW8vRaJ1kZA0L_ynyTpEW:SQG2Zl4a7IuMywKYS_eFSO_23Bc=:eyJzY29wZSI6ImtzYi11c2VyLXBob3RvIiwiZGVhZGxpbmUiOjE1MDI3MTYxMzd9';
